a4202854502d47150c73226c2815ee53b57be168
jacfrombd <jahangir.c007@gmail.com>
2025-09-10 13:47:22 +0100

    db update

---
+++ src/db/getDb.ts
@@ -0,0 +1,16 @@
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { drizzle } from "drizzle-orm/d1";
+import { cache } from "react";
+import * as schema from "./schema";
+
+// For dynamic routes
+export const getDb = cache(() => {
+    const { env } = getCloudflareContext();
+    return drizzle(env.DB, { schema });
+});
+
+// For static routes
+export const getDbAsync = cache(async () => {
+    const { env } = await getCloudflareContext({ async: true });
+    return drizzle(env.DB, { schema });
+});
